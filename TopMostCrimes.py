d7290da54f1de3f1bf8eac7e28e2f152be01f1cb
Parthivela <165021406+Parthivela@users.noreply.github.com>
2024-07-08 14:16:39 +0530

    Update TopMostCrimes.py

--- TopMostCrimes.py
+++ TopMostCrimes.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings("ignore", message= "pandas only supports SQLAlchemy connectable (engine/connection) or "
                                            "database string URI or sqlite3 DBAPI2 connection. Other DBAPI2 objects "
                                            "are not tested. Please consider using SQLAlchemy.")
-connection=pymysql.connect(host='localhost',user='root',password='Pallu@834',database='Project')
+connection=pymysql.connect(host='localhost',user='root',password='Password',database='Project')
 crime_df = pd.read_sql("select * from crime_data",connection)
 print(crime_df)
 
